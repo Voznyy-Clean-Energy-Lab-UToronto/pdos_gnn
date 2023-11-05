f22d7ee713dddabd902bd1b7087463ecdd4cf451
Ihor Neporozhnii <71763551+ineporozhnii@users.noreply.github.com>
2023-11-04 22:39:15 -0400

    convert pdos to pdos_cdf

--- utilities/data.py
+++ utilities/data.py
@@ -33,13 +33,14 @@ class MaterialData(Dataset):
     @functools.lru_cache(maxsize=None)
     def __getitem__(self, idx):
         cif_id = self.ids[idx]
-
+        
         material_graph = torch.load(self.graphs_data.extractfile(os.path.basename(self.data_file).removesuffix('.tar')+f'/{cif_id}_crystal_graph_pdos.pt'))
         material_graph.edge_attr = torch.squeeze(material_graph.edge_attr, 1)
 
         target_pdos = material_graph.pdos
         n_atoms = int(target_pdos.shape[0]/self.n_orbitals)
         atoms = []
+        e_diff = torch.mean(material_graph.e_diff)
         
         for i in range(n_atoms):
             atoms.extend([i]*self.n_orbitals)
@@ -50,5 +51,7 @@ class MaterialData(Dataset):
         material_graph.atomic_dos = atomic_dos
         material_graph.atomic_dos_cdf = atomic_dos_cdf
         material_graph.atoms_batch = atoms_batch
+        material_graph.pdos_cdf = torch.cumsum(material_graph.pdos, dim=1)*e_diff
+        material_graph.dos_cdf = torch.cumsum(material_graph.dos, dim=1)*e_diff
 
         return material_graph
