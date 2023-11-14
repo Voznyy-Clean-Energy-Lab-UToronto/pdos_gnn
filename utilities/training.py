724539750c62fac5de355780397d20ef055dc2c4
Ihor Neporozhnii <71763551+ineporozhnii@users.noreply.github.com>
2023-11-13 19:08:30 -0500

    fix division

--- utilities/training.py
+++ utilities/training.py
@@ -336,7 +336,7 @@ def train(model: ProDosNet = None,
                 output_dos_diff = torch.diff(output_dos, dim=1)/e_diff
                 output_dos_diff[output_dos_diff<0] = 0.0
                 dos_mse = mse_loss(output_dos_diff, (torch.diff(target_dos, dim=1)/e_diff)).item()
-                dos_mse_cdf = mse_loss(torch.div(output_dos, torch.max(target_dos, dim=1)[0]), torch.div(target_dos, torch.max(target_dos, dim=1)[0])).item()
+                dos_mse_cdf = mse_loss(torch.div(output_dos, torch.max(target_dos, dim=1)[0], dim=1), torch.div(target_dos, torch.max(target_dos, dim=1)[0], dim=1)).item()
 
                 output_atomic_dos_diff = torch.diff(output_atomic_dos, dim=1)/e_diff
                 output_atomic_dos_diff[output_atomic_dos_diff<0] = 0.0
@@ -608,13 +608,13 @@ def validation(model: ProDosNet = None,
          
                 output_pdos, output_atomic_dos, output_dos = model(data.x, data.edge_index, edge_attr, data.batch, data.atoms_batch, use_cdf=use_cdf, train_on_pdos=False)
 
-                loss = metric(torch.div(output_dos, torch.max(target_dos, dim=1)[0]), torch.div(target_dos, torch.max(target_dos, dim=1)[0]))
+                loss = metric(torch.div(output_dos, torch.max(target_dos, dim=1)[0], dim=1), torch.div(target_dos, torch.max(target_dos, dim=1)[0]), dim=1)
                 loss_item = loss.item()
                 
                 output_dos_diff = torch.diff(output_dos, dim=1)/e_diff
                 output_dos_diff[output_dos_diff<0] = 0.0
                 dos_mse = mse_loss(output_dos_diff, (torch.diff(target_dos, dim=1)/e_diff)).item()
-                dos_mse_cdf = mse_loss(torch.div(output_dos, torch.max(target_dos, dim=1)[0]), torch.div(target_dos, torch.max(target_dos, dim=1)[0])).item()
+                dos_mse_cdf = mse_loss(torch.div(output_dos, torch.max(target_dos, dim=1)[0], dim=1), torch.div(target_dos, torch.max(target_dos, dim=1)[0], dim=1)).item()
 
                 output_atomic_dos_diff = torch.diff(output_atomic_dos, dim=1)/e_diff
                 output_atomic_dos_diff[output_atomic_dos_diff<0] = 0.0
@@ -649,14 +649,14 @@ def validation(model: ProDosNet = None,
          
                 output_pdos, output_atomic_dos, output_dos = model(data.x, data.edge_index, edge_attr, data.batch, data.atoms_batch, use_cdf=use_cdf, train_on_pdos=False)
 
-                loss = metric(torch.div(output_dos, torch.max(target_dos, dim=1)[0]), torch.div(target_dos, torch.max(target_dos, dim=1)[0]))
+                loss = metric(torch.div(output_dos, torch.max(target_dos, dim=1)[0], dim=1), torch.div(target_dos, torch.max(target_dos, dim=1)[0], dim=1))
                 loss_item = loss.item()
             
                 dos_mse = mse_loss(output_dos, target_dos).item()
                 
                 output_dos_cdf = torch.cumsum(output_dos, dim=1)*e_diff
                 target_dos_cdf = torch.cumsum(target_dos, dim=1)*e_diff
-                dos_mse_cdf = mse_loss(torch.div(output_dos_cdf, torch.max(target_dos_cdf, dim=1)[0]), torch.div(target_dos_cdf, torch.max(target_dos_cdf, dim=1)[0])).item()
+                dos_mse_cdf = mse_loss(torch.div(output_dos_cdf, torch.max(target_dos_cdf, dim=1)[0], dim=1), torch.div(target_dos_cdf, torch.max(target_dos_cdf, dim=1)[0]), dim=1).item()
 
                 atomic_dos_mse = mse_loss(output_atomic_dos, target_atomic_dos).item()
                 atomic_dos_mse_cdf = mse_loss(torch.cumsum(output_atomic_dos, dim=1)*e_diff, torch.cumsum(target_atomic_dos, dim=1)*e_diff).item()
